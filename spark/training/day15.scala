7fd22bde5ee00f5267dc14ba3d54872eb32f4f9f
Abhishek Solanki <abhisolanki94@gmail.com>
2018-08-25 23:26:33 +0530

    day15

--- spark/training/day15.scala
+++ spark/training/day15.scala
@@ -22,3 +22,4 @@ val nTileProductsByCategory = df.select('*,ntile(3) over categoryBucket as "ntil
 
 //row_number returns a sequential number starting at 1 within a window partition.
 val rowNumByProductsCategory = df.withColumn("row_number", row_number() over categoryBucket)
+
