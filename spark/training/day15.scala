720206664d3bc79fe2f102ed0123eda1f8007311
Abhishek Solanki <abhisolanki94@gmail.com>
2018-08-27 09:37:59 +0530

    day15

--- spark/training/day15.scala
+++ spark/training/day15.scala
@@ -18,7 +18,21 @@ val denseRankProductsByCategory = df.withColumn("dense_rank",dense_rank over cat
 val percentRankProductsByCategory = df.withColumn("percent_rank",percent_rank over categoryBucket)
 
 //ntile computes the ntile group id (from 1 to n inclusive) in an ordered window partition.
-val nTileProductsByCategory = df.select('*,ntile(3) over categoryBucket as "ntile")  ' // this last quote is not needed
+val nTileProductsByCategory = df.select('*,ntile(3) over categoryBucket as "ntile")    '
 
 //row_number returns a sequential number starting at 1 within a window partition.
 val rowNumByProductsCategory = df.withColumn("row_number", row_number() over categoryBucket)
+
+// Analytic Functions
+val cumeDistProductsCatregory = df.withColumn("cume_dist", cume_dist() over categoryBucket)
+// lag function
+//Lag function allows us to compare current row with preceding rows within each partition depending on the second argument (offset) which is by default set to 1 i.e. 
+//previous row but you can change that parameter 2 to compare against every other preceding row.
+//The 3rd parameter is default value to be returned when no preceding values exists or null.
+val lagProducstCategory = df.withColumn("lag", lag('revenue, 1) over windowSpec)   '
+
+//lead function
+//Lead function allows us to compare current row with subsequent rows within each partition depending on the second argument (offset) which is by default set to 1 i.e. n
+//ext row but you can change that parameter 2 to compare against every other row.
+//The 3rd parameter is default value to be returned when no subsequent values exists or null.
+val leadProducstCategory = df.withColumn("lag", lead('revenue, 1) over windowSpec)   '
